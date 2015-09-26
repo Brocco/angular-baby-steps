bf3deaacf8b67beebb35e5acfad5e4003779eba7
Mike Brocchi <mbrocchi@gmail.com>
2015-09-25 21:03:08 -0400

    Started the conversion to TypeScript

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../app/app/typings/angularjs/angular.d.ts" />
+/// <reference path="../app/app/typings/jquery/jquery.d.ts" />
+/// <reference path="../app/typings/angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="../app/typings/angularjs/angular.d.ts" />
+/// <reference path="../app/typings/jquery/jquery.d.ts" />
