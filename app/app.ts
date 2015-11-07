72d657990a2292d74b973906f2a2b80939224592
Mike Brocchi <mbrocchi@gmail.com>
2015-11-07 15:58:41 -0500

    cleanup

--- app/app.ts
+++ app/app.ts
@@ -33,21 +33,3 @@ babySteps.config(['$stateProvider','$urlRouterProvider',function($stateProvider,
     });
 
 }]);
-
-declare var require: {
-    <T>(path: string): T;
-    (paths: string[], callback: (...modules: any[]) => void): void;
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
-};
-
-// checklist
-/*
-
-navigation - nested route(s)
-controllers
-directive
-filter
-service/factory
-http/rest?
-
- */
